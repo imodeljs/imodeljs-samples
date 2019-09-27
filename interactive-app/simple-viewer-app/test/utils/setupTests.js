4ff3456630b79592b559eba6f8be4a0650de42e5
imodeljs-admin <38288322+imodeljs-admin@users.noreply.github.com>
2019-09-27 15:49:15 -0400

    Rollback Electron version to the one supported by iModel.js  - Clean up test configuration a bit

--- interactive-app/simple-viewer-app/test/utils/setupTests.js
+++ interactive-app/simple-viewer-app/test/utils/setupTests.js
@@ -7,3 +7,20 @@
 // Similar issue mentioned in https://github.com/vuejs/vue-test-utils/issues/936
 require('jsdom-global')();
 window.Date = Date;
+
+// Global setup for chai and snapshot testing to avoid doing it in each test file
+const chai = require("chai");
+const chaiJestSnapshot = require("chai-jest-snapshot");
+
+chai.use(chaiJestSnapshot);
+
+beforeEach(function () {
+  const sourceFilePath = this.currentTest.file.replace("lib\\test", "src\\test").replace(/\.(jsx?|tsx?)$/, "");
+  const snapPath = sourceFilePath + ".snap";
+
+  chaiJestSnapshot.setFilename(snapPath);
+  chaiJestSnapshot.setTestName(this.currentTest.fullTitle());
+});
+
+// This is required by the iModel.js I18n module (specifically the i18next package).
+global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
